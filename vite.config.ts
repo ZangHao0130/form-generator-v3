f899dbcf0741eaeb46b3dd22c7a832bbc0269fb7
ZangHao <zanghao_2000@163.com>
2023-02-02 14:20:33 +0800

    refactor: remove unocss

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import Unocss from 'unocss/vite'
 import VueMacros from 'unplugin-vue-macros/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
@@ -49,10 +48,6 @@ export default defineConfig({
       dts: true,
       resolvers: [ElementPlusResolver()],
     }),
-
-    // https://github.com/antfu/unocss
-    // see unocss.config.ts for config
-    Unocss(),
   ],
 
   // https://github.com/vitest-dev/vitest
